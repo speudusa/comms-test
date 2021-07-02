9ed98c3d059a20b160d3018af4889f48bdc2a40f
Courtney Frey <66076696+speudusa@users.noreply.github.com>
2021-07-02 11:29:59 -0500

    cats

---
+++ comm-log-control/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace comm_log_control
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I like cats.");
+        }
+    }
+}
